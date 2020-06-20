7d7259a0eaa56a6653a9efeea23fe50e3d20b434
René Zeestraten <rene.zeestraten@cz.nl>
2020-06-20 16:40:15 +0200

    exercise3

--- lesson2/exercise3.py
+++ lesson2/exercise3.py
@@ -17,3 +17,31 @@ Use the .join() method to join these first three ARP entries back together as a 
 
 Write this string containing the three ARP entries out to a file named "arp_entries.txt".
 '''
+
+with open("show_arp.txt") as f:
+    contents = f.readlines()
+
+contents = contents[1:]
+
+print()
+
+from pprint import pprint
+pprint(contents)
+
+print()
+
+contents.sort()
+pprint(contents)
+
+print()
+
+new_list = contents[0:3]
+pprint(new_list)
+
+print()
+
+new_string = "\n".join(new_list)
+
+f = open("arp_entries.txt", mode="w")
+f.write(new_string)
+f.close()
