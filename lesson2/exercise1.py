5c68995915982d09f397fe39002e7dc6a14ef018
René Zeestraten <rene.zeestraten@cz.nl>
2020-06-20 15:55:00 +0200

    exercise1

--- lesson2/exercise1.py
+++ lesson2/exercise1.py
@@ -8,3 +8,14 @@ Close the file.
 Open the file a second time using a Python context manager (with statement). Read in the file contents using the .readlines() method. Print out the file contents to the screen. Also print out the type of the variable (you should have a list at this point).
 '''
 
+f = open("show_version.txt")
+contents = f.read()
+print(contents)
+print(type(contents))
+f.close()
+
+with open("show_version.txt") as f:
+    contents = f.readlines()
+    print(contents)
+    print(type(contents))
+
