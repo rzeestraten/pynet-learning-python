786e79e0bcc291119b2d6baafdbf4e1dfe41ef31
René Zeestraten <rene.zeestraten@cz.nl>
2020-06-20 16:06:39 +0200

    exercise2

--- lesson2/exercise2.py
+++ lesson2/exercise2.py
@@ -13,3 +13,24 @@ Using the .pop() method to remove the first IP address in the list and the last 
 
 Update the new first IP address in the list to be '2.2.2.2'. Print out the new first IP address in the list.
 '''
+
+ip_list = ['192.168.0.1', '192.168.0.2', '192.168.0.3', '192.168.0.4', '192.168.0.5']
+
+ip_list.append('192.168.0.6')
+
+ip_list.extend(['192.168.0.7', '192.168.0.8'])
+
+ip_list += ['192.168.0.9', '192.168.0.10']
+
+print(ip_list)
+print(ip_list[0])
+print(ip_list[9])  # better would have been to use ip_list[-1]
+
+ip_list.pop(0)
+ip_list.pop()
+
+print()
+
+ip_list[0] = '2.2.2.2'
+print(ip_list)
+print(ip_list[0])
