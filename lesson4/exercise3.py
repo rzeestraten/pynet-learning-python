26dcdd267be6b5ae884db9b5a0d3f2c6e7fab5b1
René Zeestraten <rene.zeestraten@cz.nl>
2020-07-05 20:03:35 +0200

    exercises lesson 4

---
+++ lesson4/exercise3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+'''
+Read in the 'show_version.txt' file. From this file, use regular expressions to extract the OS version, serial number, and configuration register values.
+
+Your output should look as follows:
+
+OS Version: 15.4(2)T1      
+Serial Number: FTX0000038X    
+​Config Register: 0x2102 
+
+'''
+
