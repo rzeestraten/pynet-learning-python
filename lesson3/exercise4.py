d157fb4dbd8f980cd5402130c2f22a794ffbef90
René Zeestraten <rene.zeestraten@cz.nl>
2020-07-05 18:55:41 +0200

    exercise4 completed

--- lesson3/exercise4.py
+++ lesson3/exercise4.py
@@ -26,3 +26,27 @@ Loop over this data structure and extract all of the MAC addresses. Process all 
 The hex digits should be capitalized. Additionally, there should be a colon between each octet in the MAC address.
 '''
 
+
+arp_table = [('10.220.88.1', '0062.ec29.70fe'),
+ ('10.220.88.20', 'c89c.1dea.0eb6'),
+ ('10.220.88.21', '1c6a.7aaf.576c'),
+ ('10.220.88.28', '5254.aba8.9aea'),
+ ('10.220.88.29', '5254.abbe.5b7b'),
+ ('10.220.88.30', '5254.ab71.e119'),
+ ('10.220.88.32', '5254.abc7.26aa'),
+ ('10.220.88.33', '5254.ab3a.8d26'),
+ ('10.220.88.35', '5254.abfb.af12'),
+ ('10.220.88.37', '0001.00ff.0001'),
+ ('10.220.88.38', '0002.00ff.0001'),
+ ('10.220.88.39', '6464.9be8.08c8'),
+ ('10.220.88.40', '001c.c4bf.826a'),
+ ('10.220.88.41', '001b.7873.5634')]
+
+for ip_mac in arp_table:
+    ip_addr = ip_mac[0]
+    mac_addr = ip_mac[1].upper()
+    print(f"{ip_addr}\t{mac_addr[0:2]}:{mac_addr[2:4]}:{mac_addr[5:7]}:{mac_addr[7:9]}:{mac_addr[10:12]}:{mac_addr[12:14]}")
+
+
+
+
