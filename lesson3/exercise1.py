8e9c3734529057a3a81a7f4b472573bb0e7189cc
René Zeestraten <rene.zeestraten@cz.nl>
2020-07-05 12:55:57 +0200

    exercise1 finished

--- lesson3/exercise1.py
+++ lesson3/exercise1.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import re
+from pprint import pprint
+
 '''
 Read the "show_vlan.txt" file into your program. Loop through the lines in this file and extract all of the VLAN_ID, VLAN_NAME combinations.
 From these VLAN_ID and VLAN_NAME construct a new list where each element in the list is a tuple consisting of (VLAN_ID, VLAN_NAME).
@@ -12,3 +15,18 @@ Print this data structure to the screen. Your output should look as follows:
  ('403', 'blue403')]
  '''
 
+with open("show_vlan.txt") as f:
+    contents = f.readlines()
+
+vlan_list = []
+
+for line in contents:
+    vlan_id = line.split()[0]
+    vlan_name = line.split()[1]
+
+    # use regular expression to check if variable is numeric
+    pattern = re.compile("^[0-9]+$")
+    if pattern.match(vlan_id):
+        vlan_list += [(vlan_id, vlan_name)]
+        
+pprint(vlan_list)
